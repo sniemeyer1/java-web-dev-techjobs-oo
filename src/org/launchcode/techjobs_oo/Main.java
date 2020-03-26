24b8d3329f602e989adf91376cf6816b8460938c
Savannah Niemeyer <savannahniemeyer@gmail.com>
2020-03-26 14:56:15 -0500

    array list to print jobs in main method

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -1,14 +1,25 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
         Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
-        Job job4 = new Job("", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        Job job6 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
+        //Job job4 = new Job("", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job4 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
 
-       System.out.println(job6.toString() + job1.toString() + job2.toString() + job3.toString() + job4.toString());
+        ArrayList<Job> jobs = new ArrayList<>();
+        jobs.add(job1);
+        jobs.add(job2);
+        jobs.add(job3);
+        jobs.add(job4);
+
+        for (Job job : jobs){
+            System.out.println(job);
+        }
+       //System.out.println(job1.toString() + job2.toString() + job3.toString() + job4.toString());
     }
 }
